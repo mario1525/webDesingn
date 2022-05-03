23fcd587991fcdecae572ad39dfc1dc7042706af
Mario <bla@bla.com>
2022-05-03 16:37:27 -0500

    php

---
+++ php/pratica.php
@@ -0,0 +1,19 @@
+
+    <?php
+    //mario beltran
+     $num1 = 2;
+     $num2 = 8;
+     $resul= $num1 +$num2;
+     $resulre= $num1 -$num2;
+     $resulmul= $num1 *$num2;
+     $resuldiv= $num1 /$num2;
+
+     echo "la suma e s". $resul;
+     echo "<br>";
+     echo "la resta e s". $resulre;
+     echo "<br>";
+     echo "la multi e s". $resulmul;
+     echo "<br>";
+     echo "la div e s". $resuldiv;
+?>
+  
